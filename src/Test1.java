799757c5ee09daf420ee31f05063632eaab6bdb9
Gallo94 <gallucciluca94@gmail.com>
2018-07-15 16:36:05 +0200

    add

--- src/Test1.java
+++ src/Test1.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
 
-public class Test1 {
-
+public class Test1
+{
+	public static void main (String args[])
+	{
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Nome: ");
+		String name = in.nextLine();
+		System.out.print("Età: ");
+		Integer age = in.nextInt();
+		System.out.print("Grazie");
+	}
 }
