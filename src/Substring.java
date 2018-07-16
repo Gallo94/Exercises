a4b4d670cc07fbcc1ae1dc0850f4906209eb93b1
Gallo94 <gallucciluca94@gmail.com>
2018-07-16 09:35:57 +0200

    add substring e.g.

---
+++ src/Substring.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Substring
+{
+	public static void main (String args[])
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("Write a string: ");
+		String str = in.nextLine();
+		
+		int count = 0;
+		for (int i=0; i<str.length();i++)
+		{
+			for (int j=i+1; j<=str.length();j++)
+			{
+				System.out.println(str.substring(i, j));
+				count++;
+			}
+		}
+		System.out.println();
+		System.out.println("Number occurences: " + count);
+	}
+}
